48b9faf974c658ba0c4202f3880cc5d7d22add72
Liam <duignan.liam202@gmail.com>
2022-06-12 17:58:30 +0200

    converted first name functions into single class PerExtraction

--- entity_extraction/per_extraction.py
+++ entity_extraction/per_extraction.py
@@ -1,53 +1,63 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 import os
+class PerExtraction:
 
+    def __init__(self):
+        self.names = set()
+        self.load_names_fr()
+        self.load_names_eng()
 
-def extract_names_fr():
-    """extract French first names from a wikipedia page"""
-    html_text = requests.get('https://fr.wikipedia.org/wiki/Liste_de_pr%C3%A9noms_en_fran%C3%A7ais').text
-    soup = BeautifulSoup(html_text, 'lxml')
+    def load_names_fr(self):
+        """extract French first names from a wikipedia page"""
+        html_text = requests.get('https://fr.wikipedia.org/wiki/Liste_de_pr%C3%A9noms_en_fran%C3%A7ais').text
+        soup = BeautifulSoup(html_text, 'html.parser')
 
-    wikiName = [x.find_all('a') for x in soup.find_all('div', class_='mw-parser-output')]
+        wikiName = [x.find_all('a') for x in soup.find_all('div', class_='mw-parser-output')]
 
-    list_names = []
-    for names in wikiName:
-        # add only elements that do not contain whitespaces and start with uppercase followed by lowercase
-        list_names.extend([name.text for name in names if
-                           ' ' not in name.text and re.search(r"^[A-Z][a-zà-ÿ]+", name.text) is not None])
-
-    list_names.remove(list_names[0])  # delete errant element
-    return (set(list_names))
-
-
-def extract_names_eng():
-    """extract English first names from wikipedia pages"""
-
-    # all the wikipedia pages that contain male and female English first names
-    urls = [
-        'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pageuntil=Derek#mw-pages',
-        'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pageuntil=Jodie#mw-pages',
-        'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pagefrom=Jodie#mw-pages',
-        'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pagefrom=Renssalaer#mw-pages',
-        'https://en.wikipedia.org/wiki/Category:English_feminine_given_names',
-        'https://en.wikipedia.org/w/index.php?title=Category:English_feminine_given_names&pagefrom=Drew+%28name%29#mw-pages',
-        'https://en.wikipedia.org/w/index.php?title=Category:English_feminine_given_names&pagefrom=Kirsten+%28given+name%29#mw-pages',
-        'https://en.wikipedia.org/w/index.php?title=Category:English_feminine_given_names&pagefrom=Regina+%28name%29#mw-pages']
-    list_names = []
-
-    for url in urls:
-        html_text = requests.get(url).text
-        soup = BeautifulSoup(html_text, 'lxml')
-
-        wikiName = [x.find_all('a') for x in soup.find_all('div', class_='mw-category-group')]
-
+        list_names = []
         for names in wikiName:
-            list_names.extend([name.text for name in names])
+            # add only elements that do not contain whitespaces and start with uppercase followed by lowercase
+            list_names.extend([name.text for name in names if
+                               ' ' not in name.text and re.search(r"^[A-Z][a-zà-ÿ]+", name.text) is not None])
 
-        for index, name in enumerate(list_names):
-            list_names[index] = re.sub('[\(\[].*?[\)\]]', '', name).rstrip()  # delete text within ()
+        list_names.remove(list_names[0])  # delete errant element
 
-    return set(list_names)
+        self.names = self.names.union((set(list_names)))
 
-print("Hello World")
+    def load_names_eng(self):
+        """extract English first names from wikipedia pages"""
+
+        # all the wikipedia pages that contain male and female English first names
+        urls = [
+            'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pageuntil=Derek#mw-pages',
+            'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pageuntil=Jodie#mw-pages',
+            'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pagefrom=Jodie#mw-pages',
+            'https://en.wikipedia.org/w/index.php?title=Category:English_masculine_given_names&pagefrom=Renssalaer#mw-pages',
+            'https://en.wikipedia.org/wiki/Category:English_feminine_given_names',
+            'https://en.wikipedia.org/w/index.php?title=Category:English_feminine_given_names&pagefrom=Drew+%28name%29#mw-pages',
+            'https://en.wikipedia.org/w/index.php?title=Category:English_feminine_given_names&pagefrom=Kirsten+%28given+name%29#mw-pages',
+            'https://en.wikipedia.org/w/index.php?title=Category:English_feminine_given_names&pagefrom=Regina+%28name%29#mw-pages']
+        list_names = []
+
+        for url in urls:
+            html_text = requests.get(url).text
+            soup = BeautifulSoup(html_text, 'html.parser')
+
+            wikiName = [x.find_all('a') for x in soup.find_all('div', class_='mw-category-group')]
+
+            for names in wikiName:
+                list_names.extend([name.text for name in names])
+
+            for index, name in enumerate(list_names):
+                list_names[index] = re.sub('[\(\[].*?[\)\]]', '', name).rstrip()  # delete text within ()
+
+        self.names = self.names.union(set(list_names))
+
+    def show_contents(self):
+        return self.names
+
+
+
