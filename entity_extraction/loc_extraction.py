65a781e45a4997f8a69d813503b686ee414a41a0
Liam <duignan.liam202@gmail.com>
2022-06-20 16:35:17 +0100

    added world capitals

--- entity_extraction/loc_extraction.py
+++ entity_extraction/loc_extraction.py
@@ -7,8 +7,10 @@ class LocExtraction:
 
     def __init__(self):
         self.loc = set()
+
         self.load_loc_fr()
         self.load_loc_world()
+        self.load_cap_world()
 
     def load_loc_fr(self):
         """extract French locality names from a wikipedia page"""
@@ -57,6 +59,32 @@ class LocExtraction:
         list_loc.remove(list_loc[0])  # delete errant element
 
         self.loc = self.loc.union((set(list_loc)))
+
+    def load_cap_world(self):
+        """load all world capitals from a wikipedia page"""
+
+        html_text = requests.get('https://fr.wikipedia.org/wiki/Liste_des_capitales_du_monde').text
+        soup = BeautifulSoup(html_text, 'html.parser')
+
+        table = soup.find('table', class_='wikitable sortable alternance')
+        for row in table.find_all('tr'):
+            cells = row.find_all('td')
+
+        rows = table.find_all('tr')
+
+        lst_data = []
+
+        for row in rows[2:]:
+            data = [d.text.rstrip() for d in row.find_all('td')]
+            lst_data.append(data[1])
+
+        for index, city in enumerate(lst_data):
+            lst_data[index] = re.sub('[\(\[].*?[\)\]]', '', city).rstrip()
+
+        self.loc = self.loc.union((set(lst_data)))
+
+
+
 
 
 
